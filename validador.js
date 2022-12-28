4745e9c385f7949caa130560c1558bc76671eeed
Carlos Bianchi Kremer <cbpontokremer@gmail.com>
2022-12-28 15:28:49 -0300

    validação do telefone por javascript

--- validador.js
+++ validador.js
@@ -6,10 +6,23 @@
 function mascaraTel(i){
     const tel = i.value;
     //verifica se nao é numero
-    //meio desnecessário trabalhar com vetor
+    //funciona sem usar vetor
     if(isNaN(tel[tel.length-1])){
         i.value = tel.substring(0,tel.length-1);
         return;
     }
     i.setAttribute("maxlength","16");
+    //mascara do telefone
+    if(tel.length == 1){
+        i.value = "("+i.value;
+    }
+    if(tel.length == 3){
+        i.value = i.value+") ";
+    }
+    if(tel.length == 6){
+        i.value = i.value+" ";
+    }
+    if(tel.length == 11){
+        i.value = i.value+"-";
+    }
 }
